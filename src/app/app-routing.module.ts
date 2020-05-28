98e8968052a7f4916bcc1974dc5db876881e19f9
François-Xavier Barbier <francois-xavier.barbier@edu.devinci.fr>
2020-05-28 10:35:06 +0200

    profil

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { LoginPage } from './login/login.page';
 import { RegisterPage } from './register/register.page';
 import { SearchPage } from './search/search.page';
+import { ProfilComponent } from './profil/profil.component';
 
 const routes: Routes = [
   {
@@ -13,6 +14,9 @@ const routes: Routes = [
   },
   {
     path: 'search', component: SearchPage
+  },
+  {
+    path: 'profil', component: ProfilComponent
   }
 ];
 @NgModule({
